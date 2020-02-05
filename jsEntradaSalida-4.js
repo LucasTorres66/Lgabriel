71f3c4f60237c7ceef86cb4c12cf8f9f35d36ea1
LucasTorres66 <60697981+LucasTorres66@users.noreply.github.com>
2020-02-05 12:24:15 -0300

    Add files via upload

---
+++ jsEntradaSalida-4.js
@@ -0,0 +1,14 @@
+/* 
+	Debemos lograr tomar un dato por 'PROMPT' 
+	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
+*/
+ 
+function mostrar()
+/*
+	var vnom
+vnom= prompt("Nombre","Ingrese su nombre"); 
+document.getElementById("elNombre").value= vnom ;
+}*/
+{ 
+	document.getElementById("elNombre").value= prompt("Nombre","Ingrese su nombre")
+}

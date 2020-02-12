fbf7b4d87d16b0a7310c1c4d1ac08984a457261c
LucasTorres66 <60697981+LucasTorres66@users.noreply.github.com>
2020-02-12 12:15:49 -0300

    Add files via upload

---
+++ 2-InstruccionIf/3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -0,0 +1,31 @@
+function mostrar()
+{
+//tomo la edad  
+var mesDelAnio = document.getElementById('mes').value;
+
+switch(mesDelAnio)
+{
+    case "Febrero":
+        alert ("No tiene mas de 29 dias")
+        break;
+    default:
+        alert("Tiene mas de 29 Dias");
+        /*case "Marzo":
+    case "Mayo":
+    case "Julio":
+    case "Agosto":
+    case "Octubre":
+    case "Diciembre":   
+    case "Abril":
+    case "Junio":
+    case "Septiembre":
+    case "Noviembre":
+    case "Enero":
+        alert("Tiene mas de 29 dias")
+        break;*/
+}
+	
+	
+
+
+}//FIN DE LA FUNCIÓN
